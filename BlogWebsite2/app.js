3e4931e852bfb5d57e34ebd3e726d1350b98468e
Sunil Kumar Pradhan <sunilkumarweb47@gmail.com>
2024-08-20 20:50:03 +0530

    Added template in Bog Website 2

---
+++ BlogWebsite2/app.js
@@ -0,0 +1,9 @@
+const express =require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const homeStartingContent="";
+const aboutContent="";
+const contactContent="";
+
+const app
